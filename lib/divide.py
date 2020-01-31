9d79eaafbd1d1bd6b82538289b4a6a1e4042393d
xryuseix <ryusei143.shootingstar@gmail.com>
2020-02-01 00:04:46 +0900

    mp3 to wav

--- lib/divide.py
+++ lib/divide.py
@@ -1,4 +1,5 @@
 from pydub import AudioSegment
+from tqdm import tqdm
 import pathlib
 import re
 import os
@@ -17,19 +18,22 @@ def trim(dir, file, output_root):
         for i in range(0,int(duration)-1):
             trimed_sound = sound[i*1000 : (i + 1)*1000]
             # output
-            trimed_sound.export(output_root + dir + '/' + str(i) + '.mp3', format='mp3')
+            trimed_sound.export(output_root + dir + '/' + str(i) + '.wav', format='wav')
 
 def search(raw_root, output_root):
     path = pathlib.Path(raw_root)
     persons = [str(p) for p in path.iterdir()]
 
-    for person in persons:
+    for person in tqdm(persons):
         personal_name = re.sub('^.*/', '', person)
         voices = [str(p) for p in pathlib.Path(person).iterdir()]
         for voice in voices:
             trim(personal_name, voice, output_root)
 
-raw_root = './../../../学習データ/radio_audio/trimed/'
-output_root = './lib/voice/'
+raw_root = './../../../../学習データ/radio_audio/trimed/'
+output_root = './voice/'
+
+if os.path.exists('../../../../学習データ/radio_audio/trimed/.DS_Store'):
+    os.system('rm ../../../../学習データ/radio_audio/trimed/.DS_Store')
 
 search(raw_root, output_root)
